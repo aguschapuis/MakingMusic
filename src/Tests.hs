57dc72e6481602d3961b57eab2abae9900c04bdc
Chapuis <aguschapuis@gmail.com>
2020-07-27 20:18:23 -0300

    nuevos test para el punto estrella

--- src/Tests.hs
+++ src/Tests.hs
@@ -7,6 +7,9 @@ import Command
 s1 :: Song
 s1 = Fragment [Note qn (A, 1), Note qn (B, 1), Note qn (C, 1)]
 
+s5 :: Song
+s5 = Transpose_by 2 (Fragment [Note qn (A, 3), Note qn (B, 4), Note qn (C, 3)])
+
 s3 :: Song
 s3 = Fragment [Note qn (C, 2), Note qn (D, 2), Note qn (Ds, 2), Rest qn, Note qn (G, 2)]
 
@@ -20,7 +23,8 @@ chord_g :: Song
 chord_g = Parallel (Fragment [Note qn (G, 3)]) (Fragment [Note qn (D, 3)])
 
 s2 :: Song
-s2 = Repeat 2 (Transpose_by 2 (Concat (Repeat 4 chord_c) (Repeat 4 chord_g)))
+s2 = Transpose_by 4 ((Concat (Repeat 4 chord_c) (Repeat 4 chord_g)))
+
 
 add_chord_c = Add (Parallel (Fragment [Note qn (C, 3)]) (Fragment [Note qn (G, 3)]))
 add_chord_g = Add (Parallel (Fragment [Note qn (G, 3)]) (Fragment [Note qn (D, 3)]))
@@ -39,3 +43,26 @@ testTime = time s2
 
 testRun :: Maybe Song
 testRun = run [add_chord_c, add_chord_g, join_chords] [] >>= \r-> return (r!!0)
+
+--- ejemplos para testear el Punto Estrella
+
+p1 :: Song
+p1 = parallelMin s1 s3
+
+p2 :: Song
+p2 = parallelMax s1 s2
+
+--- Toma 2 canciones y devuelve true si el time de la cancion mas corta  
+--- es igual al del resultado de aplicarle parallelMin a las dos
+
+testParallelMin :: Song -> Song -> Bool
+testParallelMin sng1 sng2 | (time sng1) < (time sng2) = ((time sng1) == (time (parallelMin sng1 sng2)))
+                          | (time sng2) <= (time sng1) = ((time sng2) == (time (parallelMin sng1 sng2)))
+
+
+--- Toma 2 canciones y devuelve true si el time de la cancion mas larga  
+--- es igual al del resultado de aplicarle parallelMax a las dos
+
+testParallelMax :: Song -> Song -> Bool
+testParallelMax sng1 sng2 | (time sng1) > (time sng2) = ((time sng1) == (time (parallelMax sng1 sng2)))
+                          | (time sng2) >= (time sng1) = ((time sng2) == (time (parallelMax sng1 sng2)))
